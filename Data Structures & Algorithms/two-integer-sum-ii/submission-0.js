a9bb813113acaacb63f5a0bf5b6ae3505017f4c9
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-14 23:38:42 -0300

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let right = numbers.length - 1;
+        let left = 0;
+
+        while (left < right) {
+            const sum = numbers[right] + numbers[left];
+            if (sum == target) return [left + 1, right + 1];
+            if (sum > target) {
+                right--;
+                continue;
+            }
+            left++;
+        }
+    }
+}
