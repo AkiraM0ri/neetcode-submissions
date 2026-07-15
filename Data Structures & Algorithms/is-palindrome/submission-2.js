e08eb81eeb6ee21fbaf0605c15b4c4d40eb865ac
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-14 23:23:54 -0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    // time complexity: O(n)
+    // space complexity: O(n)
+    isPalindrome(s) {
+        s = s.toLowerCase()
+        let strWithoutSpace = s.replace(/[^a-z0-9]/gi, '')
+        let right = strWithoutSpace.length - 1
+        let left = 0
+        
+        while(right > left) {
+            if(strWithoutSpace[left] !== strWithoutSpace[right]) return false
+            
+            left++
+            right--
+        }
+
+        return true
+    }
+}
