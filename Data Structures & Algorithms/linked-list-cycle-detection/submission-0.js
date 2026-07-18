6924207da85a2a03b1e3671e470ca3480c594534
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-18 18:43:30 -0300

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let hashmap = new Set();
+        let root = head;
+
+        while (root !== null) {
+            if (hashmap.has(root)) return true;
+            hashmap.add(root);
+            root = root.next;
+        }
+
+        return false;
+    }
+}
