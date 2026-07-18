7ff98bb3d48a85bc668ae13c215cce5cbf13dc17
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-18 15:15:22 -0300

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const prefix = new Array(nums.length).fill(1);
+        const suffix = new Array(nums.length).fill(1);
+
+        for (let i = 1; i < nums.length; i++) {
+            prefix[i] = nums[i - 1] * prefix[i - 1];
+        }
+
+        for (let i = nums.length - 1; i > 0; i--) {
+            suffix[i - 1] = nums[i] * suffix[i];
+        }
+
+        return suffix.map((elm, i) => elm * prefix[i]);
+    }
+}
