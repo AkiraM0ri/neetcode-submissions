0b2c0aa9f92d18500fb2ef70316607772584b784
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-24 17:11:11 -0300

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const str = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+        let l = 0;
+        let r = str.length - 1;
+        while (l < r) {
+            if (str[l] !== str[r]) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
