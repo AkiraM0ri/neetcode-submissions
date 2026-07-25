313cd4178fa5e0a6536972acf7ae0359bf3e7de7
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-07-25 19:18:31 -0300

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const stack = [];
+        const res = new Array(temperatures.length).fill(0);
+
+        for (let i = 0; i < temperatures.length; i++) {
+
+            while (stack.length && temperatures[stack.at(-1)] < temperatures[i]) {
+                const top = stack.pop();
+                res[top] = i - top;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
