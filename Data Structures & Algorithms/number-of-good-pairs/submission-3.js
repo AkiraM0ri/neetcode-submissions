2dc95041f8149143b79c8a33a79c8a2405f86970
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-08-07 11:03:27 -0300

    Add: number-of-good-pairs - submission-3

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    // runtime: O(n) | space: O(n)
+    numIdenticalPairs(nums) {
+        const frequency = new Map();
+        let anwser = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            const number = nums[i];
+            anwser += frequency.get(number) || 0;
+
+            frequency.set(number, (frequency.get(number) || 0) + 1);
+        }
+
+        return anwser;
+    }
+}
