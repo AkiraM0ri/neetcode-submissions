da44a92b8965829ec2aa37cf6f6c0e0abdaf437e
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-08-07 10:46:48 -0300

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    numIdenticalPairs(nums) {
+        let res = 0
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) res++
+            }
+        }
+
+        return res
+    }
+}
