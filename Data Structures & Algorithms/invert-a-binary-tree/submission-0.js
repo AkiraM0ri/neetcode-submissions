afa176be912daadaa19b0be5bc501557610881bf
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-08-11 23:17:40 -0300

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        function traversal(node) {
+            if (node == null) return null;
+
+            const temp = node.left
+            node.left = node.right
+            node.right = temp
+
+            traversal(node.left);
+            traversal(node.right);
+
+            return node
+        }
+
+        return traversal(root);
+    }
+}
