8d29f479b304354912af6e02aee5974e8dc6c1ff
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-08-28 14:18:18 -0300

    Add: jump-game - submission-6

---
+++ Data Structures & Algorithms/jump-game/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    // T: O(n) | S: O(1)
+    canJump(nums) {
+        const target = nums.length - 1;
+
+        let highestJump = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (i > highestJump) return false;
+
+            highestJump = Math.max(highestJump, i + nums[i]);
+            if (highestJump >= target) return true;
+        }
+
+        return false;
+    }
+}
