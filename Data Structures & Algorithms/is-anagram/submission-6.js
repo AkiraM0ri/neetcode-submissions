b756c4c90b52ca3054e80d73e9d4558f6c1ad41d
Nicolas Akira <88866025+AkiraM0ri@users.noreply.github.com>
2026-08-31 12:07:20 -0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    // T: O(n) | S: O(n)
+    isAnagram(s, t) {
+        const frequency = new Map();
+        for (let char of t) {
+            frequency.set(char, (frequency.get(char) || 0) + 1);
+        }
+
+        for (let char of s) {
+            if (!frequency.has(char)) return false;
+
+            frequency.set(char, (frequency.get(char) || 0) - 1);
+            if(frequency.get(char) <= 0) frequency.delete(char)
+        }
+
+        return !frequency.size
+    }
+}
